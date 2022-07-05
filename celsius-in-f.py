e7e4fbd529f494c9b853b6396afb2c287b76a043
Jhoseph Cabral <remakeraps@hotmail.com>
2022-07-05 17:22:35 -0300

    celsius in f convert

---
+++ celsius-in-f.py
@@ -0,0 +1,10 @@
+celsius = float(input("Digite uma temperatura em Celsius: "))
+
+f = (celsius * 9/5) + 32
+
+print("Em Fh: ", f)
+celsius = float(input("Digite uma temperatura em Celsius: "))
+
+f = (celsius * 9/5) + 32
+
+print("Em Fh: ", f)
